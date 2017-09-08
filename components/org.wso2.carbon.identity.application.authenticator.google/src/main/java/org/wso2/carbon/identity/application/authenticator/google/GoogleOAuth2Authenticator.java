b80a6c91a55b62d719d87482b8d21dce7d52b730
Supun Priyadarshana <supunp@wso2.com>
2017-09-08 16:51:21 +0530

    Added dedicated claim dialect support without changing the authentication framework  (#20)
    * Added dedicated claim dialect uri support
    
    * Address reviewed issues
    
    * Addressed the reviewed issues

--- components/org.wso2.carbon.identity.application.authenticator.google/src/main/java/org/wso2/carbon/identity/application/authenticator/google/GoogleOAuth2Authenticator.java
+++ components/org.wso2.carbon.identity.application.authenticator.google/src/main/java/org/wso2/carbon/identity/application/authenticator/google/GoogleOAuth2Authenticator.java
@@ -17,21 +17,32 @@
  */
 package org.wso2.carbon.identity.application.authenticator.google;
 
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONValue;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.oltu.oauth2.client.response.OAuthClientResponse;
+import org.apache.oltu.oauth2.common.utils.JSONUtils;
+import org.wso2.carbon.identity.application.authentication.framework.config.builder.FileBasedConfigurationBuilder;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.AuthenticatorConfig;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authenticator.oidc.OIDCAuthenticatorConstants;
 import org.wso2.carbon.identity.application.authenticator.oidc.OpenIDConnectAuthenticator;
+import org.wso2.carbon.identity.application.common.model.ClaimMapping;
 import org.wso2.carbon.identity.application.common.model.Property;
 import org.wso2.carbon.identity.application.common.util.IdentityApplicationConstants;
+import org.wso2.carbon.identity.base.IdentityConstants;
+import org.wso2.carbon.identity.core.util.IdentityCoreConstants;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 public class GoogleOAuth2Authenticator extends OpenIDConnectAuthenticator {
 
     private static final long serialVersionUID = -4154255583070524018L;
+    private static Log log = LogFactory.getLog(GoogleOAuth2Authenticator.class);
     private String tokenEndpoint;
     private String oAuthEndpoint;
     private String userInfoURL;
@@ -214,5 +225,116 @@ public class GoogleOAuth2Authenticator extends OpenIDConnectAuthenticator {
     @Override
     public String getName() {
         return GoogleOAuth2AuthenticationConstant.GOOGLE_CONNECTOR_NAME;
+    }
+
+    @Override
+    public String getClaimDialectURI() {
+        String claimDialectUri = super.getClaimDialectURI();
+        AuthenticatorConfig authConfig = FileBasedConfigurationBuilder.getInstance().getAuthenticatorBean(getName());
+        if (authConfig != null) {
+           Map<String, String> parameters = authConfig.getParameterMap();
+           if (parameters != null && parameters.containsKey(GoogleOAuth2AuthenticationConstant.
+                  CLAIM_DIALECT_URI_PARAMETER)) {
+               claimDialectUri = parameters.get(GoogleOAuth2AuthenticationConstant.CLAIM_DIALECT_URI_PARAMETER);
+           } else {
+               if (log.isDebugEnabled()) {
+                   log.debug("Found no Parameter map for connector " + getName());
+               }
+           }
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("FileBasedConfigBuilder returned null AuthenticatorConfigs for the connector " +
+                        getName());
+            }
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Authenticator " + getName() + " is using the claim dialect uri " + claimDialectUri);
+        }
+        return claimDialectUri;
+    }
+
+    @Override
+    protected void buildClaimMappings(Map<ClaimMapping, String> claims, Map.Entry<String, Object> entry, String separator) {
+        String claimValue = null;
+        String claimUri   = "";
+        if (StringUtils.isBlank(separator)) {
+            separator = IdentityCoreConstants.MULTI_ATTRIBUTE_SEPARATOR_DEFAULT;
+        }
+        try {
+            JSONArray jsonArray = (JSONArray) JSONValue.parseWithException(entry.getValue().toString());
+            if (jsonArray != null && jsonArray.size() > 0) {
+                Iterator attributeIterator = jsonArray.iterator();
+                while (attributeIterator.hasNext()) {
+                    if (claimValue == null) {
+                        claimValue = attributeIterator.next().toString();
+                    } else {
+                        claimValue = claimValue + separator + attributeIterator.next().toString();
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            claimValue = entry.getValue().toString();
+        }
+        String claimDialectUri = getClaimDialectURI();
+        if (!OIDCAuthenticatorConstants.OIDC_CLAIM_DIALECT_URI.equals(claimDialectUri)) {
+            claimUri = claimDialectUri + "/";
+        }
+
+        claimUri += entry.getKey();
+        claims.put(ClaimMapping.build(claimUri, claimUri, null, false), claimValue);
+        if (log.isDebugEnabled() && IdentityUtil.isTokenLoggable(IdentityConstants.IdentityTokens.USER_CLAIMS)) {
+            log.debug("Adding claim mapping : " + claimUri + " <> " + claimUri + " : " + claimValue);
+        }
+    }
+
+    /**
+     * Get subject attributes.
+     * @param token OAuthClientResponse
+     * @param authenticatorProperties Map<String, String> (Authenticator property, Property value)
+     * @return Map<ClaimMapping, String> Claim mappings.
+     */
+    protected Map<ClaimMapping, String> getSubjectAttributes(OAuthClientResponse token,
+                                                             Map<String, String> authenticatorProperties) {
+
+        Map<ClaimMapping, String> claims = new HashMap<>();
+
+        try {
+            String accessToken = token.getParam(OIDCAuthenticatorConstants.ACCESS_TOKEN);
+            String url = getUserInfoEndpoint(token, authenticatorProperties);
+            String json = sendRequest(url, accessToken);
+
+            if (StringUtils.isBlank(json)) {
+                if(log.isDebugEnabled()) {
+                    log.debug("Empty JSON response from user info endpoint. Unable to fetch user claims." +
+                            " Proceeding without user claims");
+                }
+                return claims;
+            }
+
+            Map<String, Object> jsonObject = JSONUtils.parseJSON(json);
+
+            for (Map.Entry<String, Object> data : jsonObject.entrySet()) {
+                String key = data.getKey();
+                Object value = data.getValue();
+                String claimDialectUri = getClaimDialectURI();
+                if (!OIDCAuthenticatorConstants.OIDC_CLAIM_DIALECT_URI.equals(claimDialectUri)) {
+                    key = claimDialectUri + "/" + key;
+                }
+                if (value != null) {
+                    claims.put(ClaimMapping.build(key, key, null, false), value.toString());
+                }
+
+                if (log.isDebugEnabled() && IdentityUtil.isTokenLoggable(IdentityConstants.IdentityTokens.USER_CLAIMS)
+                        && jsonObject.get(key) != null) {
+                    log.debug("Adding claims from end-point data mapping : " + key + " - " + jsonObject.get(key)
+                            .toString());
+                }
+            }
+        } catch (IOException e) {
+            log.error("Communication error occurred while accessing user info endpoint", e);
+        }
+
+        return claims;
     }
 }
